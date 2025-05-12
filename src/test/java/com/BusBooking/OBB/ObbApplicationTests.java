7f1fb8cb3cdabbd094e4741962642f15a3bdf3ff
Akhi114 <akhilla76@gmail.com>
2025-05-12 14:24:43 +0530

    commit 4

--- src/test/java/com/BusBooking/OBB/ObbApplicationTests.java
+++ src/test/java/com/BusBooking/OBB/ObbApplicationTests.java
@@ -1,13 +1,13 @@
 package com.BusBooking.OBB;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ObbApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ObbApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
